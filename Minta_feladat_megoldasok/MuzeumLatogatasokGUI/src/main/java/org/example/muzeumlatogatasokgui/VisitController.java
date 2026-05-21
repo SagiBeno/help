86224c473b79e87f6fb24e65638657928ef239cb
SagiBeno <benosagi321@gmail.com>
2026-05-21 22:19:35 +0200

    stream() használata

--- Minta_feladat_megoldasok/MuzeumLatogatasokGUI/src/main/java/org/example/muzeumlatogatasokgui/VisitController.java
+++ Minta_feladat_megoldasok/MuzeumLatogatasokGUI/src/main/java/org/example/muzeumlatogatasokgui/VisitController.java
@@ -15,22 +15,22 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.text.Collator;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class VisitController {
 
-    public static ArrayList<Visit> visits = new ArrayList<>();
+    @FXML private VBox container;
+    @FXML private HBox radioButtonsContainer;
+    @FXML private ListView<String> dataListView;
+    @FXML private ListView<String> resultListView;
+    @FXML private RadioButton citiesRadioButton;
+    @FXML private RadioButton typesRadioButton;
 
-    @FXML public VBox container;
-    @FXML public HBox radioButtonsContainer;
-    @FXML public ListView<String> dataListView;
-    @FXML public ListView<String> resultListView;
-    @FXML public RadioButton citiesRadioButton;
-    @FXML public RadioButton typesRadioButton;
+    private static List<Visit> visits = new ArrayList<>();
+    private static final Collator collator = Collator.getInstance(new Locale("hu", "HU"));
 
-    public static Collator collator = Collator.getInstance(new Locale("hu", "HU"));
-
-    @FXML public void handleFileOpener() {
+    @FXML private void handleFileOpener() {
         Stage currentStage = (Stage) container.getScene().getWindow();
 
         try {
@@ -45,17 +45,16 @@ public class VisitController {
                 citiesRadioButton.setSelected(true);
                 showCities();
             }
-        }
-        catch (Exception e) {
-            System.out.println(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
-    @FXML protected void handleExit() {
+    @FXML private void handleExit() {
         Platform.exit();
     }
 
-    @FXML protected void handleAbout () {
+    @FXML private void handleAbout () {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Névjegy");
         alert.setHeaderText("");
@@ -63,58 +62,58 @@ public class VisitController {
         alert.showAndWait();
     }
 
-    @FXML public void showCities() {
-        resultListView.getItems().clear();
-        ObservableList<String> citiesNames = FXCollections.observableList(new ArrayList<>());
+    @FXML private void showCities() {
+        resultListView.setItems(null);
 
-        for (Visit visit : visits) {
-            if (!citiesNames.contains(visit.getCity())) citiesNames.add(visit.getCity());
-        }
+        ObservableList<String> citiesNames = FXCollections.observableList(
+                visits.stream()
+                        .map(Visit::getCity)
+                        .distinct()
+                        .sorted(collator)
+                        .toList()
+        );
 
-        citiesNames.sort(collator);
         dataListView.setItems(citiesNames);
     }
 
-    @FXML public void showTypes() {
-        resultListView.getItems().clear();
-        ObservableList<String> types = FXCollections.observableList(new ArrayList<>());
+    @FXML private void showTypes() {
+        resultListView.setItems(null);
 
-        for (Visit visit : visits) {
-            if (!types.contains(visit.getType())) types.add(visit.getType());
-        }
+        ObservableList<String> types = FXCollections.observableList(
+                visits.stream()
+                        .map(Visit::getType)
+                        .distinct()
+                        .sorted(collator)
+                        .toList()
+        );
 
-        types.sort(collator);
         dataListView.setItems(types);
     }
 
-    @FXML public void handleShowDetails() {
+    @FXML private void handleShowDetails() {
         String data = dataListView.getSelectionModel().getSelectedItem();
         if (citiesRadioButton.isSelected()) handleShowCityResult(data);
         if (typesRadioButton.isSelected()) handleShowTypeResult(data);
     }
 
-    @FXML public void handleShowCityResult(String city) {
-        ObservableList<String> visitsObList = FXCollections.observableList(new ArrayList<>());
-
-        for (Visit visit : visits) {
-            if (visit.getCity().equalsIgnoreCase(city)) {
-                String data = visit.getVisitName() + " | " + visit.getMuseumName() + " | " + visit.getType() + " | " + visit.getVisitTime() + " óra";
-                visitsObList.add(data);
-            }
-        }
+    @FXML private void handleShowCityResult(String city) {
+        ObservableList<String> visitsObList = FXCollections.observableList(
+                visits.stream()
+                        .filter(visit -> visit.getCity().equalsIgnoreCase(city))
+                        .map(visit -> visit.getVisitName() + " | " + visit.getMuseumName() + " | " + visit.getType() + " | " + visit.getVisitTime() + " óra")
+                        .toList()
+        );
 
         resultListView.setItems(visitsObList);
     }
 
-    @FXML public void handleShowTypeResult(String type) {
-        ObservableList<String> visitsObList = FXCollections.observableList(new ArrayList<>());
-
-        for (Visit visit : visits) {
-            if (visit.getType().equalsIgnoreCase(type)) {
-                String data = visit.getVisitName() + " | " + visit.getMuseumName() + " | " + visit.getCity() + " | " + visit.getVisitTime() + " óra";
-                visitsObList.add(data);
-            }
-        }
+    @FXML private void handleShowTypeResult(String type) {
+        ObservableList<String> visitsObList = FXCollections.observableList(
+                visits.stream()
+                        .filter(visit -> visit.getType().equalsIgnoreCase(type))
+                        .map(visit -> visit.getVisitName() + " | " + visit.getMuseumName() + " | " + visit.getCity() + " | " + visit.getVisitTime() + " óra")
+                        .toList()
+        );
 
         resultListView.setItems(visitsObList);
     }
