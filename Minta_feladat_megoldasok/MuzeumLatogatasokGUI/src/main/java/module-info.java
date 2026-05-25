083b050d85022f3d03abdd798a6824af0e1bf22a
SagiBeno <benosagi321@gmail.com>
2026-05-25 16:29:00 +0200

    Mappa törlése

--- Minta_feladat_megoldasok/MuzeumLatogatasokGUI/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.muzeumlatogatasokgui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.muzeumlatogatasokgui to javafx.fxml;
-    exports org.example.muzeumlatogatasokgui;
-}
